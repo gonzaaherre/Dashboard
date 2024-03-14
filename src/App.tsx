bc88e522359c40fc17783f0232c82803790cac9a
gonzaaherre <gonzaaherre@gmail.com>
2024-03-14 19:32:26 -0300

    ...

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+ import CarrouselHome from "./components/CarrouselHome/Carrousel"
+import Dashboard from "./components/Dashboard/Dashboard"
+import Header from "./components/Header/Header"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/> 
+      <Dashboard/>
+      <CarrouselHome/>
+      
+    </>
+  )
+}
+
+export default App
